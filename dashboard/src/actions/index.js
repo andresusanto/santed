bc90e66ff71b1f1e59e9ab07a7eb360f7a71e2d0
Andre Susanto <andre@susan.to>
2017-07-21 22:12:08 +0700

    [DASHBOARD] Implement filterbox and graphql client (#5)

--- dashboard/src/actions/index.js
+++ dashboard/src/actions/index.js
@@ -1,19 +1,34 @@
 export const openHeaderMenu = title => {
-  return {
-    type: 'OPEN_HEADER_MENU',
-    title,
-  }
+    return {
+        type: 'OPEN_HEADER_MENU',
+        title,
+    }
 };
 
 export const closeHeaderMenu = () => {
-  return {
-    type: 'CLOSE_HEADER_MENU',
-  }
+    return {
+        type: 'CLOSE_HEADER_MENU',
+    }
 };
 
 export const updateDocumentTitle = (title) => {
-  return {
-    type: 'UPDATE_TITLE',
-    title,
-  }
+    return {
+        type: 'UPDATE_TITLE',
+        title,
+    }
 };
+
+export const createWorkforce = () => {
+    const query = `
+        users {
+            id,
+            username,
+            email,
+            threads
+        }`;
+    
+    return {
+        type: 'CREATE_WORK_FORCE',
+        query,
+    }
+}
--- dashboard/src/pages/workforce/index.js
+++ dashboard/src/pages/workforce/index.js
@@ -2,23 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { updateDocumentTitle } from '../../actions';
 import ContentBox from '../../components/contentbox';
+import FilterBox from '../../components/filterbox';
+import Button from '../../components/button';
+import Table from '../../components/table';
+import {
+    updateDocumentTitle,
+    createWorkforce,
+} from '../../actions';
 
 
 class WorkforcePage extends Component {
+    static contextTypes = {
+        gql: PropTypes.object,
+    };
     static propTypes = {
         dispatch: PropTypes.func.isRequired,
     };
 
     componentWillMount() {
         this.props.dispatch(updateDocumentTitle('Workforce'));
+        this.context.gql.dispatch(createWorkforce());
     }
 
     render() {
         return (
-            <ContentBox title="Workforce">
-                Force gan!
+            <ContentBox title="Workforce" headerAction={(
+                <Button title="Create" icon="plus" />
+            )}>
+                <FilterBox />
+                <Table />
             </ContentBox>
         );
     }
